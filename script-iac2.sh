f4f93982191d6da7c56481c2a50d6c4ebdaec5bb
diegosystem1 <diego_system@outlook.com>
2022-08-11 00:26:59 +0000

    Apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Atualizando o servirdor..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get  install unzip -y
+
+echo "Baixando e copiando arquivos do github..."
+
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -r * /var/www/html/
